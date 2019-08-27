aa75846aad9ae00d10f4a50382f1f2942515c1c3
Sai <saikumart13@gmail.com>
2019-08-27 20:04:32 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,32 +1,44 @@
-def check(*b):
-    num_count,alp_count = [0,0]
+def check(b):
+    num_count,alp_count,lis_count = [0,0,0]
     for el in b:
-       temp = str(el)
-       if(temp.isdigit()== True):
+       temp = el
+       if(el[0].isdigit()== True):
            num_count += 1
-       elif(temp.isalpha()== True):
+       elif(el[0].isprintable()== True):
            alp_count += 1
+       elif(type(el[0])== list):
+           lis_count += 1
        else:
            None
-    if(num_count == 0 and alp_count == len(b)):
+       # if(temp.isdigit()== True):
+       #     num_count += 1
+       # elif(temp.isalpha()== True):
+       #     alp_count += 1
+       # elif(type(temp)==list):
+       #     lis_count += 1
+       # else:
+       #     None
+    if(alp_count == len(b)):
         return ''
-    elif(alp_count == 0 and num_count == len(b)):
+    elif(num_count == len(b)):
         return 0
+    elif(lis_count == len(b)):
+        return []
     else:
         return -1
 
-def add(*a):
-    sum = check(*a)
+def add(a):
+    sum = check(a)
     if sum == -1:
         return print("Given inputs are of different types.")
     elif sum == 0:
         for var in a:
-            sum += int(var)
+            sum += int(var[0])
     else:
         for var in a:
-            sum += var
+            sum += var[0]
     print(sum)
 
 #add('gh','hh')
-*elem = input('Enter elements to be added:').split()
+elem = [[x] for x in input('Enter elements to be added:').split()]
 add(elem)
